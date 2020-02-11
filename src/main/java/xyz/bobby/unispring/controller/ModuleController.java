62da00474c8c7920552bd9530b0382f0287b0596
Nebojsa Cvetkovic <nebkat@gmail.com>
2020-02-11 16:52:57 +0000

    modules: controller

---
+++ src/main/java/xyz/bobby/unispring/controller/ModuleController.java
@@ -0,0 +1,33 @@
+package xyz.bobby.unispring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import xyz.bobby.unispring.exception.ResourceNotFoundException;
+import xyz.bobby.unispring.model.Module;
+import xyz.bobby.unispring.repository.ModuleRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class ModuleController {
+	@Autowired
+	private ModuleRepository moduleRepository;
+
+	@GetMapping("/module")
+	public List<Module> getModules() {
+		return moduleRepository.findAll();
+	}
+
+	@PostMapping(value = "/module", consumes = MediaType.ALL_VALUE)
+	public Module createModule(@Valid @RequestBody Module module) {
+		return moduleRepository.save(module);
+	}
+
+	@GetMapping(value = "/modules/{id}")
+	public Module getModule(@PathVariable("id") int id) throws ResourceNotFoundException {
+		return moduleRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException(Module.class.getSimpleName(), id));
+	}
+}
