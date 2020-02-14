b8e7a4913a87d421f56ea589ea8cac223c93294c
debattlinbeen <cjbeenham@gmail.com>
2020-02-14 15:53:05 +0000

    util: year converter for db

---
+++ src/main/java/xyz/bobby/unispring/util/YearConverter.java
@@ -0,0 +1,18 @@
+package xyz.bobby.unispring.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.time.Year;
+
+@Converter(autoApply = true)
+public class YearConverter implements AttributeConverter<Year, Short> {
+	@Override
+	public Short convertToDatabaseColumn(Year attribute) {
+		return (short) attribute.getValue();
+	}
+
+	@Override
+	public Year convertToEntityAttribute(Short dbValue) {
+		return Year.of(dbValue);
+	}
+}
