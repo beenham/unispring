6b68b7c69cda199d3afe81a65b6aefafdc455b77
Mason <masonsmith40@hotmail.co.uk>
2020-03-07 02:11:03 +0000

    dashboard: complete

--- src/main/webapp/src/dashboard.js
+++ src/main/webapp/src/dashboard.js
@@ -1,7 +1,7 @@
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import {Bar, Doughnut, Pie, Bubble} from 'react-chartjs-2';
-import DashBoardStatDisplay from "./dashboardStatDisplay";
+import DashboardStat from "./dashboardStat";
 
 var colours = [
     'rgb(0, 152, 224, 0.8)',
@@ -19,14 +19,10 @@ var border_colours = [
     '#F78979',
     '#B86377'
 ];
-
-function strMapToObj(strMap) {
-    let obj = Object.create(null);
-    for (let [k,v] of strMap) {
-        obj[k] = v;
-    }
-    return obj;
+function getcolour(num){
+    return colours[num];
 }
+
 
 function studentStageNumbers(json_data){
     let dataMap = new Map();
@@ -36,10 +32,37 @@ function studentStageNumbers(json_data){
         }else{
             dataMap.set(item.stage, dataMap.get(item.stage) + 1);
         }
-
     }
-    // console.log(json_data)
-    return strMapToObj(dataMap);
+    return dataMap;
+}
+
+async function getGradeData(data){
+
+    let dataMap = new Map();
+    let student_grades = [];
+    let student_grades_data = [];
+
+    for (const gradeData of data) {
+        if(!dataMap.has(gradeData.grade)){
+            dataMap.set(gradeData.grade,0);
+        }else{
+            dataMap.set(gradeData.grade, dataMap.get(gradeData.grade) + 1);
+        }
+    }
+    for (let [key, value] of dataMap) {
+        student_grades.push(key);
+        student_grades_data.push(value);
+    }
+
+    return  {
+        labels: student_grades,
+        datasets: [{
+            label: 'No. of students who achieved this grade',
+            data: student_grades_data,
+            backgroundColor: colours,
+            borderColor: "#fff",
+            borderWidth: 1
+        }]};
 }
 
 async function getGenderData(data){
@@ -63,112 +86,58 @@ async function getGenderData(data){
     return  {
         labels: student_genders,
         datasets: [{
-            label: '# of Votes',
             data: student_genders_data,
             backgroundColor: colours,
-            borderColor: border_colours,
+            borderColor: "#fff",
             borderWidth: 1
         }]};
 }
 
+function getNationalityData(data){
+    let dataMap = new Map();
+    let countries = [];
+    let country_data = [];
 
-function Dashboard(){
+    let data_list = [];
 
-    var colours = [
-        'rgb(0, 152, 224, 0.8)',
-        'rgb(94, 188, 219, 0.8)',
-        'rgb(241, 221, 132, 0.8)',
-        'rgb(245, 181, 135, 0.8)',
-        'rgb(247, 137, 121, 0.8)',
-        'rgb(184, 99, 119, 0.8)'
-    ];
-    var border_colours = [
-        '#0098E0',
-        '#3C778B',
-        '#F1DD84',
-        '#F5B587',
-        '#F78979',
-        '#B86377'
-    ];
+    for (const student of data) {
+        if(!dataMap.has(student.nationality)){
+            dataMap.set(student.nationality,0);
+        }else{
+            dataMap.set(student.nationality, dataMap.get(student.nationality) + 1);
+        }
+    }
 
-    var text_colour  = "#404040";
+    for (let [key, value] of dataMap) {
+        countries.push(key);
+        country_data.push(value);
+    }
 
-
-    var bar_data= {
-        labels: ['NG - E', 'D', 'C', 'B', 'A'],
-        datasets: [{
-            label: 'Grade Break down',
-            data: [2, 8, 13, 20, 13],
-            backgroundColor: colours,
-            borderColor: border_colours,
-            borderWidth: 1
-        }]
-    };
-
-    var data_doughnut = {
-        labels: ['Male', 'Female', 'Other'],
-        datasets: [{
-            label: '# of Votes',
-            data: [25, 15, 13],
-            backgroundColor: colours,
-            borderColor: border_colours,
-            borderWidth: 1
-        }]
-    };
-
-    var data_pie = {
-        labels: ['Male', 'Female', 'Other'],
-        datasets: [{
-            label: '# of Votes',
-            data: [25, 15, 13],
-            backgroundColor: colours,
-            borderColor: border_colours,
-            borderWidth: 1
-        }]
-    };
-
-    var data_bubble = {
-        labels: "Computer Science",
-        datasets: [
+    for(let i = 0; i < countries.length; i++){
+        data_list.push(
             {
-                label: ["China"],
-                backgroundColor:'#0098E0',
-                borderColor:'#0098E0',
+                label: [countries[i]],
+                backgroundColor:getcolour(i),
+                borderColor:'#fff',
                 data: [{
-                    x: 190,
-                    y: 5.245,
-                    r: 18
-                }]
-            }, {
-                label: ["India"],
-                backgroundColor:'#0098E0',
-                borderColor:'#0098E0',
-                data: [{
-                    x: 200,
-                    y: 7.526,
-                    r: 20
-                }]
-            }, {
-                label: ["Ireland"],
-                backgroundColor:'#0098E0',
-                borderColor:'#0098E0',
-                data: [{
-                    x: 250,
-                    y: 6.994,
-                    r: 24
-                }]
-            }, {
-                label: ["Poland"],
-                backgroundColor: '#0098E0',
-                borderColor: '#0098E0',
-                data: [{
-                    x: 100,
-                    y: 5.921,
-                    r: 15
+                    x: country_data[i],
+                    y: 5,
+                    r: (40*(country_data[i])/100)
                 }]
             }
-        ]
+        );
+    }
+
+    return {
+        labels: "Computer Science",
+        datasets: data_list,
+        options:{
+            order:1
+        }
     };
+}
+
+function Dashboard(){
 
     useEffect(() => {
         fetchStudents();
@@ -181,12 +150,23 @@ function Dashboard(){
         let data_j = data_json._embedded.students.filter(function(item){return item;});
 
         const data_json_staff =  await fetch('http://localhost:8080/api/staff/?size=250').then(res => res.json());
-        console.log(data_json_staff);
         let data_j_staff = data_json_staff._embedded.staff.filter(function(item){return item;});
 
-        data_json["numbers"] = studentStageNumbers(data_json._embedded.students);
+        const data_json_grades =  await fetch('http://localhost:8080/api/grades/?size=250').then(res => res.json());
+        let data_j_grades = data_json_grades._embedded.grades.filter(function(item){return item;});
+
+        let stageMap = studentStageNumbers(data_json._embedded.students);
+
+        data_json["stage_one"] = stageMap.get("ONE");
+        data_json["stage_two"] = stageMap.get("TWO");
+        data_json["stage_three"] = stageMap.get("THREE");
+        data_json["stage_four"] = stageMap.get("FOUR");
+        data_json["stage_M"] = stageMap.get("MASTERS");
+        data_json["stage_D"] = stageMap.get("DOCTORATE");
         data_json["studentGenderBreakDown"] = await getGenderData(data_j);
         data_json["staffGenderBreakDown"] = await getGenderData(data_j_staff);
+        data_json["gradesBreakdown"] = await getGradeData(data_j_grades);
+        data_json["nationalityBreakdown"] = getNationalityData(data_j);
         setItems(data_json);
     };
 
@@ -205,13 +185,12 @@ function Dashboard(){
             </div>
         </section>
         <nav className="level">
-            {/*{items.map(item =>(*/}
-            {/*    */}
-            {/*    <DashboardStat name="fds" number={"fdx"}/>*/}
-            {/*))}*/}
-
-            <DashBoardStatDisplay data={items.numbers}/>
-
+            <DashboardStat name="Stage One" number={items.stage_one}/>
+            <DashboardStat name="Stage Two" number={items.stage_two}/>
+            <DashboardStat name="Stage Three" number={items.stage_three}/>
+            <DashboardStat name="Stage Four" number={items.stage_four}/>
+            <DashboardStat name="Masters" number={items.stage_M}/>
+            <DashboardStat name="Doctorate" number={items.stage_D}/>
         </nav>
 
         <div className="tile is-ancestor">
@@ -220,32 +199,28 @@ function Dashboard(){
                     <div className="tile is-parent">
                         <article className="tile is-child box">
                             <p className="title is-4">Student Gender Breakdown</p>
-                            {/* <canvas id="chart-area" ></canvas> */}
                             <Pie data={items.studentGenderBreakDown} id="chart-area"/>
                         </article>
                     </div>
                     <div className="tile is-parent">
                         <article className="tile is-child box">
                             <p className="title is-4">Staff Gender Breakdown</p>
-                            {/* <canvas id="chart-area2" ></canvas> */}
                             <Doughnut data={items.staffGenderBreakDown} />
                         </article>
                     </div>
                 </div>
                 <div className="tile is-parent ">
                     <article className="tile is-child box">
-                        <p className="title is-4">One</p>
-                        {/* <canvas id="bubble-chart" width="100" height="30"></canvas> */}
-                        <Bubble data={data_bubble} width={100} height={30}/>
+                        <p className="title is-4">Student Nationality Breakdown</p>
+                        <Bubble data={items.nationalityBreakdown} width={100} height={30}/>
                     </article>
                 </div>
             </div>
             <div className="tile is-parent">
                 <article className="tile is-child box">
                     <div className="content">
-                        <p className="title is-4">One</p>
-                        {/* <canvas id="myChart" width="100" height="140"></canvas> */}
-                        < Bar data={bar_data} id="myChart" width={100} height={145}/>
+                        <p className="title is-4">Average Grade Break Down (Undergraduate)</p>
+                        < Bar data={items.gradesBreakdown} id="myChart" width={100} height={145}/>
                     </div>
                 </article>
             </div>
